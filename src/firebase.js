f30b69363f81693b62273add2232f48a9744b9ef
MR-KRAMA <khanderam4@gmail.com>
2025-06-22 10:50:44 +0530

    adding the .env file

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,15 @@ import { toast } from "react-toastify";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkmdENXgX6KC-5I3pPTg6Zz4PpYUejFos",
-  authDomain: "netdlix-clone-47215.firebaseapp.com",
-  projectId: "netdlix-clone-47215",
-  storageBucket: "netdlix-clone-47215.firebasestorage.app",
-  messagingSenderId: "487172466354",
-  appId: "1:487172466354:web:143cbe7f44cc7e29a0cbab",
-  measurementId: "G-15E7WVXPZB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
